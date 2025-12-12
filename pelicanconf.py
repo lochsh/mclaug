fa87e7a1360e4b6cd470b934781a6899647f7c8f
Hannah McLaughlin <h@mcla.ug>
2025-12-12 15:55:10 +0000

    update long bio

--- pelicanconf.py
+++ pelicanconf.py
@@ -40,10 +40,22 @@ LONG_BIO = (
        "I'm an engineer with an amateur interest in linguistics. I live in Oxford, UK, "
        "and I love learning about phonetics and phonology. I'm particularly interested "
        "in Japanese and Gaelic, especially the Gaelic of Donegal.<br><br>"
+
+       "So much of the richness of spoken language is in the specific sounds and "
+       "phrases that tie it to a particular place, and to the people who have lived "
+       "there. I love learning local words and pronunciations, and seeing how these "
+       "demonstrate both the unique history of a place, and its connection to other "
+       "places, whether near or far. <br><br>"
+
        "I have previously worked as a software engineer, writing Rust and Python for "
        "quantum computers, C++ for medical imaging diagnostics, and bare metal "
        "embedded C for surgical robots.<br><br>"
-       "I studied Information and Computer Engineering at the University of Cambridge."
+
+       "I studied Information and Computer Engineering at the University of "
+       "Cambridge.<br><br>"
+
+       "Most of the posts on my blog have been about software and related technology. "
+       "I look forward to writing more about languages."
 )
 NAME_EN = "I'm Hannah"
 NAME_JA = "ハナといいます"
