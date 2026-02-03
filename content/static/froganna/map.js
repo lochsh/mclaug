298a314b74c294d9c9e4893b3eb2ced48fa41e19
Hannah McLaughlin <h@mcla.ug>
2026-02-03 21:37:24 +0000

    wrap dynamic html gen in fn

--- content/static/froganna/map.js
+++ content/static/froganna/map.js
@@ -1,185 +1,156 @@
-var map = L.map('map').setView([55.2, -5.3], 6);
-var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+$(document).ready(function () {
+    var map = L.map('map').setView([55.2, -5.3], 6);
+    var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-var container = document.getElementById("map-container");
-var clustering = L.markerClusterGroup();
-map.addLayer(clustering);
+    var container = document.getElementById("map-container");
+    var clustering = L.markerClusterGroup();
+    map.addLayer(clustering);
 
-$.get(
-    "../static/froganna/data/words.csv",
-    function(data) {
-        var div = document.createElement("fieldset");
-        div.id = "words";
-        div.className = "inputs";
+    function makeCheckboxes(basename, classname, listClass, legendTitle, addToggle) {
+        $.get(
+            `../static/froganna/data/${basename}.csv`,
+            function(data) {
+                var div = document.createElement("fieldset");
+                div.id = classname;
+                div.className = "inputs";
 
-        var legend = document.createElement("legend");
-        legend.innerHTML = "Words";
-        div.appendChild(legend);
+                var legend = document.createElement("legend");
+                legend.innerHTML = legendTitle;
+                div.appendChild(legend);
 
-        var list = document.createElement("ul");
-        list.className = "checkboxes columns";
-        div.appendChild(list);
+                var list = document.createElement("ul");
+                list.className = listClass;
+                div.appendChild(list);
 
-        var words = $.csv.toObjects(data);
-        for (word of words) {
-            var input = document.createElement("input");
-            input.className = "word";
-            input.type = "checkbox";
-            input.value = word.key;
-            input.setAttribute("checked", "true");
+                var lines = $.csv.toObjects(data);
+                for (line of lines) {
+                    var input = document.createElement("input");
+                    input.className = classname;
+                    input.type = "checkbox";
+                    input.value = line.key;
+                    input.setAttribute("checked", "true");
 
-            var label = document.createElement("label");
-            label.setAttribute("for", word.key);
-            label.innerHTML = word.display;
+                    var label = document.createElement("label");
+                    label.setAttribute("for", line.key);
+                    label.innerHTML = line.display;
 
-            var item = document.createElement("li");
-            item.appendChild(input);
-            item.appendChild(label);
+                    var item = document.createElement("li");
+                    item.appendChild(input);
+                    item.appendChild(label);
 
-            list.appendChild(item);
-        }
+                    list.appendChild(item);
+                }
 
-        var toggleAll = document.createElement("button");
-        toggleAll.id = "toggle-all-words";
-        toggleAll.innerHTML = "Toggle all";
-        div.appendChild(toggleAll);
+                if (addToggle) {
+                    var toggleAll = document.createElement("button");
+                    toggleAll.id = "toggle-all-words";
+                    toggleAll.innerHTML = "Toggle all";
+                    div.appendChild(toggleAll);
+                }
 
-        container.appendChild(div);
-    }
-);
-
-$.get(
-    "../static/froganna/data/sources.csv",
-    function(data) {
-        var div = document.createElement("fieldset");
-        div.id = "sources";
-        div.className = "inputs";
-
-        var legend = document.createElement("legend");
-        legend.innerHTML = "Sources";
-        div.appendChild(legend);
-
-        var list = document.createElement("ul");
-        list.className = "checkboxes";
-        div.appendChild(list);
-
-        var sources = $.csv.toObjects(data);
-        for (source of sources) {
-            var input = document.createElement("input");
-            input.className = "source";
-            input.type = "checkbox";
-            input.value = source.key;
-            input.setAttribute("checked", "true");
-
-            var label = document.createElement("label");
-            label.setAttribute("for", source.key);
-            label.innerHTML = source.display;
-
-            var item = document.createElement("li");
-            item.appendChild(input);
-            item.appendChild(label);
-
-            list.appendChild(item);
-        }
-
-        container.appendChild(div);
-    }
-);
-
-function onEachFeature(feature, marker) {
-    marker.bindPopup(
-        `<b>${feature.properties.word}</b><br>
-        <i>Source transcription</i>: <span class=ipa>${feature.properties.source_transcription}</span><br>
-        <i>IPA transcription</i>: <span class=ipa>${feature.properties.ipa_transcription}</span><br><br>
-        <i>Year</i>: ${feature.properties.year}<br>
-        <i>Informant</i>: ${feature.properties.informant}<br><br>
-        <i>Place</i>: ${feature.properties.logainm} / ${feature.properties.placename}<br>
-        <i>Area</i>: ${feature.properties.ceantar} / ${feature.properties.area}<br><br>
-        <i>Source</i>: ${feature.properties.source}<br><br>
-        <i>Notes</i>: ${feature.properties.note}<br>`,
-        {maxHeight: 500},
-    ).openPopup();
-}
-
-// thanks to https://jsfiddle.net/newluck77/rk9v0uyo/
-var checkboxStates = {
-    sources: [],
-    words: []
-};
-
-const geojsonLayer = L.geoJSON(
-    null,
-    {
-        filter: (feature) => {
-            const isSourceChecked = checkboxStates.sources.includes(
-                feature.properties.source_category
-            );
-            const isWordChecked = checkboxStates.words.includes(
-                feature.properties.category
-            );
-            return isSourceChecked && isWordChecked;
-        },
-        onEachFeature: onEachFeature,
-    }
-).addTo(clustering)
-
-function updateCheckboxStates() {
-    checkboxStates = {
-        sources: [],
-        words: []
-    };
-    for (let input of document.querySelectorAll("input")) {
-        if(input.checked) {
-            switch (input.className) {
-                case "source":
-                    checkboxStates.sources.push(input.value);
-                    break;
-                case "word":
-                    checkboxStates.words.push(input.value);
-                    break;
-            }
-        }
-    }
-}
-
-function updateMap(data) {
-    clustering.clearLayers();
-    geojsonLayer.clearLayers();
-    updateCheckboxStates();
-    geojsonLayer.addData(data).addTo(clustering);
-}
-
-$.getJSON(
-    "../static/froganna/data/frogs.json",
-    function(frogData) {
-
-        for (let input of document.querySelectorAll("input")) {
-            input.onchange = (e) => {
-                updateMap(frogData);
-            }
-        }
-
-        // Initialise map
-        updateMap(frogData);
-
-        $(document).ready(
-            function() {
-                $("#map-container").on(
-                    "click",
-                    "#toggle-all-words",
-                    function() {
-                        if ($(".word:checked").length == $(".word").length) {
-                            $(".word").prop("checked", false);
-                        } else {
-                            $(".word").prop("checked", true);
-                        }
-                        updateMap(frogData);
-                     }
-                );
+                container.appendChild(div);
             }
         );
     }
-);
+
+    makeCheckboxes("words", "word", "checkbox columns", "Words", true);
+    makeCheckboxes("sources", "source", "checkbox", "Sources", false);
+
+    function onEachFeature(feature, marker) {
+        marker.bindPopup(
+            `<b>${feature.properties.word}</b><br>
+            <i>Source transcription</i>: <span class=ipa>${feature.properties.source_transcription}</span><br>
+            <i>IPA transcription</i>: <span class=ipa>${feature.properties.ipa_transcription}</span><br><br>
+            <i>Year</i>: ${feature.properties.year}<br>
+            <i>Informant</i>: ${feature.properties.informant}<br><br>
+            <i>Place</i>: ${feature.properties.logainm} / ${feature.properties.placename}<br>
+            <i>Area</i>: ${feature.properties.ceantar} / ${feature.properties.area}<br><br>
+            <i>Source</i>: ${feature.properties.source}<br><br>
+            <i>Notes</i>: ${feature.properties.note}<br>`,
+            {maxHeight: 500},
+        ).openPopup();
+    }
+
+    // thanks to https://jsfiddle.net/newluck77/rk9v0uyo/
+    var checkboxStates = {
+        sources: [],
+        words: []
+    };
+
+    const geojsonLayer = L.geoJSON(
+        null,
+        {
+            filter: (feature) => {
+                const isSourceChecked = checkboxStates.sources.includes(
+                    feature.properties.source_category
+                );
+                const isWordChecked = checkboxStates.words.includes(
+                    feature.properties.category
+                );
+                return isSourceChecked && isWordChecked;
+            },
+            onEachFeature: onEachFeature,
+        }
+    ).addTo(clustering)
+
+    function updateCheckboxStates() {
+        checkboxStates = {
+            sources: [],
+            words: []
+        };
+        for (let input of document.querySelectorAll("input")) {
+            if(input.checked) {
+                switch (input.className) {
+                    case "source":
+                        checkboxStates.sources.push(input.value);
+                        break;
+                    case "word":
+                        checkboxStates.words.push(input.value);
+                        break;
+                }
+            }
+        }
+    }
+
+    function updateMap(data) {
+        clustering.clearLayers();
+        geojsonLayer.clearLayers();
+        updateCheckboxStates();
+        geojsonLayer.addData(data).addTo(clustering);
+    }
+
+    $.getJSON(
+        "../static/froganna/data/frogs.json",
+        function(frogData) {
+
+            for (let input of document.querySelectorAll("input")) {
+                input.onchange = (e) => {
+                    updateMap(frogData);
+                }
+            }
+
+            // Initialise map
+            updateMap(frogData);
+
+            $(document).ready(
+                function() {
+                    $("#map-container").on(
+                        "click",
+                        "#toggle-all-words",
+                        function() {
+                            if ($(".word:checked").length == $(".word").length) {
+                                $(".word").prop("checked", false);
+                            } else {
+                                $(".word").prop("checked", true);
+                            }
+                            updateMap(frogData);
+                         }
+                    );
+                }
+            );
+        }
+    );
+});
